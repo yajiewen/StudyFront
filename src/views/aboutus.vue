48de3c59e99fe6cc87d9bfb64b332c831a1a8213
yajiewen <909092921@qq.com>
2021-05-31 13:09:50 +0800

    add new base url

--- src/views/aboutus.vue
+++ src/views/aboutus.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="aboutus" class="pdiv">
-<!--    <div class="columns">
-      <div class="column is-half is-offset-5">
-        <img src="../assets/nav.jpg" width="200" >
-      </div>
-    </div>-->
-    <div class="imgp">
+<!--    <div class="imgp">
       <img src="../assets/nav.jpg" width="200" >
-    </div>
+    </div>-->
     <div class="columns">
     <div class="column is-half is-offset-3">
       <div class="card">
-        <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp我们是厦门大学STUDY创业团队，为解决家教供需双方的痛点，我们研发了师生智能匹配的家教平台，让大学生勤工俭学实践的同时，提高孩子学习能力，为孩子创造更美好的未来。</p>
+<!--        <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp我们是厦门大学STUDY创业团队，为解决家教供需双方的痛点，我们研发了师生智能匹配的家教平台，让大学生勤工俭学实践的同时，提高孩子学习能力，为孩子创造更美好的未来。</p>
         <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp一.核心理念：STUDY平台致力于安全优质家教，做双方满意的平台。</p>
         <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp二.产品定位：大学家教市场。</p>
-        <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp三.优势：收费低廉且透明、家教安全且满意、AI匹配且高效。</p>
+        <p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp三.优势：收费低廉且透明、家教安全且满意、AI匹配且高效。</p>-->
+        <p>我们是厦门大学STUDY创业团队，为解决家教供需双方的痛点，我们研发了师生智能匹配的家教平台，让大学生勤工俭学实践的同时，提高孩子学习能力，为孩子创造更美好的未来。</p>
+        <p>一.核心理念：STUDY平台致力于安全优质家教，做双方满意的平台。</p>
+        <p>二.产品定位：大学家教市场。</p>
+        <p>三.优势：收费低廉且透明、家教安全且满意、AI匹配且高效。</p>
       </div>
     </div>
     </div>
